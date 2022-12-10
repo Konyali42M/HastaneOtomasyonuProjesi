217ea866df307f7d62751176acfe4db61bfe5a9a
Ömer Yavaş <siyah.inek2003@gmail.com>
2022-12-10 20:57:36 +0300

    Update Address.java
    Address Class'ı yazıldı.

--- src/Address.java
+++ src/Address.java
@@ -1,2 +1,35 @@
 public class Address {
+    private String street;
+    private String town;
+    private String city;
+
+    public Address(String street, String town, String city) {
+        this.street = street;
+        this.town = town;
+        this.city = city;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getTown() {
+        return town;
+    }
+
+    public void setTown(String town) {
+        this.town = town;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
 }
