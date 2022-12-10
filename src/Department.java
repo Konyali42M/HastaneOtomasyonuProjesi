22e263e8d6abafc74cd69d4899c9255e9e02e53a
Ömer Yavaş <siyah.inek2003@gmail.com>
2022-12-10 21:03:53 +0300

    Update Department.java
    Department Class'ı yazıldı.

--- src/Department.java
+++ src/Department.java
@@ -1,2 +1,38 @@
 public class Department {
+    private String id;
+    private String name;
+    private Room[] rooms;
+
+    public Department(String id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Room[] getRooms() {
+        return rooms;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void addRoom (Room r) {
+
+    }
+
+    public void listRooms() {
+
+    }
 }
