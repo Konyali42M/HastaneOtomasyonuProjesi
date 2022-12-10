b368e8842cb7091f8767eea5003ddc7783044191
Ömer Yavaş <siyah.inek2003@gmail.com>
2022-12-10 20:41:28 +0300

    Update Room.java
    Room Class'ı yazıldı.

--- src/Room.java
+++ src/Room.java
@@ -1,2 +1,25 @@
 public class Room {
+    private String roomId;
+    private String name;
+
+    public Room(String roomId, String name){
+        this.roomId = roomId;
+        this.name = name;
+    }
+
+    public String getRoomId() {
+        return roomId;
+    }
+
+    public void setRoomId(String roomId) {
+        this.roomId = roomId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
